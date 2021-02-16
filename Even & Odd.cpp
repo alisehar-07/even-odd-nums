fcfec3f9a349c6707f5ef8a1ada82294cbb0f57f
Ali Sehar <77107640+alisehar-07@users.noreply.github.com>
2021-02-16 16:34:18 +0500

    Add files via upload

---
+++ Even & Odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"Enter The Number :";
+	cin>>num;
+	if (num%2==0){
+	cout<<"You Enter An Even Number :"<<num;
+	}else {
+	cout<<"You Enter An Odd Number :"<<num;
+	}
+}
